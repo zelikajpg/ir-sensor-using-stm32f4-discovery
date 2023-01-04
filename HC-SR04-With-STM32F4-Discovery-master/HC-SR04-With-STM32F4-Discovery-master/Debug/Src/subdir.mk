d27a8e40f0c6ef356e6cb5fd5588bd9ea0e644ed
zelika <78971864+zelikajpg@users.noreply.github.com>
2023-01-03 21:46:14 -0800

    ir sensor with stm32f4 discovery

---
+++ HC-SR04-With-STM32F4-Discovery-master/HC-SR04-With-STM32F4-Discovery-master/Debug/Src/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/dwt_stm32_delay.c \
+../Src/main.c \
+../Src/stm32f4xx_hal_msp.c \
+../Src/stm32f4xx_it.c \
+../Src/syscalls.c \
+../Src/system_stm32f4xx.c 
+
+OBJS += \
+./Src/dwt_stm32_delay.o \
+./Src/main.o \
+./Src/stm32f4xx_hal_msp.o \
+./Src/stm32f4xx_it.o \
+./Src/syscalls.o \
+./Src/system_stm32f4xx.o 
+
+C_DEPS += \
+./Src/dwt_stm32_delay.d \
+./Src/main.d \
+./Src/stm32f4xx_hal_msp.d \
+./Src/stm32f4xx_it.d \
+./Src/syscalls.d \
+./Src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc -c "$<" -mcpu=cortex-m4 -std=gnu11 -g3 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F407xx -c -I../Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/dwt_stm32_delay.d ./Src/dwt_stm32_delay.o ./Src/dwt_stm32_delay.su ./Src/main.d ./Src/main.o ./Src/main.su ./Src/stm32f4xx_hal_msp.d ./Src/stm32f4xx_hal_msp.o ./Src/stm32f4xx_hal_msp.su ./Src/stm32f4xx_it.d ./Src/stm32f4xx_it.o ./Src/stm32f4xx_it.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/system_stm32f4xx.d ./Src/system_stm32f4xx.o ./Src/system_stm32f4xx.su
+
+.PHONY: clean-Src
+
